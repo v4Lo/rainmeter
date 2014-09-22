efe5d6e38eda6ac4adf8e3faf06bb1ad2d413205
Valo <valo1@web.de>
2014-09-22 05:48:14 +0200

    adds ipv6 support to ping plugin

--- Plugins/PluginPing/Ping.cpp
+++ Plugins/PluginPing/Ping.cpp
@@ -14,18 +14,19 @@
   You should have received a copy of the GNU General Public License
   along with this program; if not, write to the Free Software
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
+  */
 
 #include <windows.h>
-#include <Winsock2.h>
 #include <string>
 #include <Ipexport.h>
 #include <Icmpapi.h>
+#include "ws2tcpip.h"
 #include "../../Library/Export.h"	// Rainmeter's exported functions
+
 
 struct MeasureData
 {
-	IPAddr destAddr;
+	struct addrinfo* destAddr;
 	DWORD timeout;
 	double timeoutValue;
 	DWORD updateRate;
@@ -36,7 +37,7 @@ struct MeasureData
 	void* skin;
 
 	MeasureData() :
-		destAddr(),
+		destAddr(nullptr),
 		timeout(),
 		timeoutValue(),
 		updateRate(),
@@ -89,32 +90,18 @@ PLUGIN_EXPORT void Reload(void* data, void* rm, double* maxValue)
 		{
 			char* buffer = new char[bufLen];
 			WideCharToMultiByte(CP_ACP, 0, value, strLen, buffer, bufLen, nullptr, nullptr);
-
-			measure->destAddr = inet_addr(buffer);
-			if (measure->destAddr == INADDR_NONE)
-			{
-				WSADATA wsaData;
-				if (WSAStartup(0x0101, &wsaData) == 0)
-				{
-					LPHOSTENT pHost = gethostbyname(buffer);
-					if (pHost)
-					{
-						measure->destAddr = *(DWORD*)pHost->h_addr;
-					}
-					else
-					{
-						RmLog(LOG_WARNING, L"PingPlugin.dll: Unable to get host by name");
-					}
-
-					WSACleanup();
+			int error;
+			WORD wVersionRequested = MAKEWORD(2, 2);
+			WSADATA wsaData;
+			if (WSAStartup(wVersionRequested, &wsaData) == 0) {
+				if ((error = getaddrinfo(buffer, NULL, NULL, &measure->destAddr)) != 0){
+						{
+							RmLogF(nullptr, LOG_WARNING, L"PingPlugin.dll: getaddrinfo failed %d", error);
+						}
 				}
-				else
-				{
-					RmLog(LOG_WARNING, L"PingPlugin.dll: Unable to start WSA");
-				}
+				WSACleanup();
 			}
-
-			delete [] buffer;
+			delete[] buffer;
 		}
 	}
 
@@ -130,25 +117,64 @@ DWORD WINAPI NetworkThreadProc(void* pParam)
 	// NOTE: Do not use CRT functions (since thread was created by CreateThread())!
 
 	MeasureData* measure = (MeasureData*)pParam;
-	const DWORD bufferSize = sizeof(ICMP_ECHO_REPLY) + 32;
-	BYTE buffer[bufferSize];
+
 
 	double value = 0.0;
-	HANDLE hIcmpFile = IcmpCreateFile();
-	if (hIcmpFile != INVALID_HANDLE_VALUE)
-	{
-		IcmpSendEcho(hIcmpFile, measure->destAddr, nullptr, 0, nullptr, buffer, bufferSize, measure->timeout);
-		IcmpCloseHandle(hIcmpFile);
+	if (measure->destAddr != nullptr) {
+		if (measure->destAddr->ai_family == AF_INET6) {
+			HANDLE hIcmpFile = Icmp6CreateFile();
+			if (hIcmpFile != INVALID_HANDLE_VALUE) {
+				struct sockaddr_in6 source;
+				memset(&source, 0, sizeof(source));
+				source.sin6_addr = in6addr_any;
+				source.sin6_family = AF_INET6;
+				
+				sockaddr_in6 dest;
+				memset(&dest, 0, sizeof(dest));
+				dest.sin6_addr = ((sockaddr_in6 *)measure->destAddr->ai_addr)->sin6_addr;
+				dest.sin6_family = AF_INET6;
+				
+				const int REQUEST_DATA_SIZE = 6;
+				char* requestData = new char[REQUEST_DATA_SIZE];
+				strcpy(requestData, "HELLO");
+				const int reply_data_size = sizeof(ICMPV6_ECHO_REPLY) + REQUEST_DATA_SIZE;
+				BYTE buffer[reply_data_size];
+				Icmp6SendEcho2(hIcmpFile, 0, 0, 0, &source, &dest, requestData, 5, nullptr, buffer, reply_data_size, measure->timeout);
+				IcmpCloseHandle(hIcmpFile);
 
-		ICMP_ECHO_REPLY* reply = (ICMP_ECHO_REPLY*)buffer;
-		value = (reply->Status != IP_SUCCESS) ? measure->timeoutValue : reply->RoundTripTime;
+				ICMPV6_ECHO_REPLY* reply = (ICMPV6_ECHO_REPLY*)buffer;
+				value = (reply->Status != IP_SUCCESS) ? measure->timeoutValue : reply->RoundTripTime;
+				delete[] requestData;
+				if (!measure->finishAction.empty())
+				{
+					RmExecute(measure->skin, measure->finishAction.c_str());
+				}
 
-		if (!measure->finishAction.empty())
-		{
-			RmExecute(measure->skin, measure->finishAction.c_str());
+			}
+		}
+		else if (measure->destAddr->ai_family == AF_INET) {
+			HANDLE hIcmpFile = IcmpCreateFile();
+			if (hIcmpFile != INVALID_HANDLE_VALUE)
+			{
+				in_addr inaddr = ((sockaddr_in *)measure->destAddr->ai_addr)->sin_addr;
+				IPAddr ipaddr;
+				memcpy(&ipaddr, &inaddr, sizeof(ipaddr));
+				const DWORD bufferSize = sizeof(ICMP_ECHO_REPLY) + 32;
+				BYTE buffer[bufferSize];
+
+				IcmpSendEcho(hIcmpFile, ipaddr, nullptr, 0, nullptr, buffer, bufferSize, measure->timeout);
+				IcmpCloseHandle(hIcmpFile);
+
+				ICMP_ECHO_REPLY* reply = (ICMP_ECHO_REPLY*)buffer;
+				value = (reply->Status != IP_SUCCESS) ? measure->timeoutValue : reply->RoundTripTime;
+
+				if (!measure->finishAction.empty())
+				{
+					RmExecute(measure->skin, measure->finishAction.c_str());
+				}
+			}
 		}
 	}
-
 	HMODULE module = nullptr;
 
 	EnterCriticalSection(&g_CriticalSection);
@@ -161,6 +187,7 @@ DWORD WINAPI NetworkThreadProc(void* pParam)
 	{
 		// Thread is not attached to an existing measure any longer, so delete
 		// unreferenced data.
+		freeaddrinfo(measure->destAddr);
 		delete measure;
 
 		DWORD flags = GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT;
@@ -174,7 +201,6 @@ DWORD WINAPI NetworkThreadProc(void* pParam)
 		// the last instance.
 		FreeLibraryAndExitThread(module, 0);
 	}
-
 	return 0;
 }
 
